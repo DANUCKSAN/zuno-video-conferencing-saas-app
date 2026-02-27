90444cb4034f6cf0904d30f3bdc52bc8a99e7d2e
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-02-27 11:07:56 +1100

    setup-done-revised

---
+++ components/Sidebar.tsx
@@ -0,0 +1,19 @@
+import { sidebarLinks } from '@/constants'
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+        <div className='flex flex-col gap-6'>
+            {sidebarLinks.map((link,index) => (
+                <div key={index} className='flex cursor-pointer items-center gap-3 text-sm font-medium'>
+                    <span>{link.label}</span>
+                </div>
+            ))}
+        </div>
+
+    </section>
+  )
+}
+
+export default Sidebar
