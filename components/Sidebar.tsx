a8c80bf857ace25c8d69b79fec81389533b644c4
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-02-27 12:14:16 +1100

    sidebar-done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,15 +1,39 @@
+'use client'
 import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Sidebar = () => {
+    const pathname=usePathname();
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
         <div className='flex flex-col gap-6'>
-            {sidebarLinks.map((link,index) => (
-                <div key={index} className='flex cursor-pointer items-center gap-3 text-sm font-medium'>
-                    <span>{link.label}</span>
-                </div>
-            ))}
+            {sidebarLinks.map((item)=>{
+                const isActive=pathname===item.route || pathname.startsWith(`${item.route}/`)
+                return (
+                  <Link 
+                  href={item.route}
+                  key={item.label}
+                  className={cn(
+                    'flex gap-4 items-center p-4 rounded-lg justify-start',{
+                        'bg-blue-1':isActive,   
+                    }
+                  )}>
+                    <Image
+                    src={item.imgURL}
+                    width={24}
+                    height={24}
+                    alt={item.label}/>
+
+                    <p className='text-lg font-semibold max-lg:hidden'>
+                        {item.label}
+                    </p>
+                  </Link>
+                )
+            })}
         </div>
 
     </section>
