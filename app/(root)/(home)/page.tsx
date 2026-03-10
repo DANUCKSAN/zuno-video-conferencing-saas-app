a230a32f434a6b066d550cd33366d214fe112f51
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-03-10 14:42:16 +1100

    home-init

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-   <section className='flex flex-col items-center justify-center h-screen gap-6'>
-    <h1 className='text-4xl font-bold text-white'>Welcome to Zuno</h1>
-    <p className='text-lg text-gray-300'>Your Zoom clone built with Next.js, Tailwind CSS, and WebRTC.</p>
+ <section className='flex size-full flex-col gap-10' text-white>
+  <div className='h-[300px] w-full rounded-[20px] bg-hero bg-hover'>
+   <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+    <h2 className='glassmorphism text-4xl text-white font-bold max-md:text-2xl'>Experience Seamless Video Conferencing with Zuno</h2>
+    <p className='text-lg text-white max-md:text-sm'>Join the future of online meetings with Zuno, the ultimate video conferencing app designed for effortless communication and collaboration.</p>
+   </div>
+  </div>
 
-   </section>
+ </section>
   )
 }
 
