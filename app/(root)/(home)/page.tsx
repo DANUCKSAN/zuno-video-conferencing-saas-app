6fdae100af30a1b8e925b1af53242704f08fc389
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-03-11 15:55:24 +1100

    homecards-done

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
+'use client';
+
+import MeetingTypeList from "@/components/MeetingTypeList";
+
 
 const Home = () => {
-  return (
- <section className='flex size-full flex-col gap-10' text-white>
-  <div className='h-[300px] w-full rounded-[20px] bg-hero bg-hover'>
-   <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-    <h2 className='glassmorphism text-4xl text-white font-bold max-md:text-2xl'>Experience Seamless Video Conferencing with Zuno</h2>
-    <p className='text-lg text-white max-md:text-sm'>Join the future of online meetings with Zuno, the ultimate video conferencing app designed for effortless communication and collaboration.</p>
-   </div>
-  </div>
+  const now = new Date();
 
- </section>
-  )
-}
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
+
+      <MeetingTypeList/>
+    </section>
+  );
+};
 
 export default Home;
