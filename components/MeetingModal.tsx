ef4ae55aa213d51011d41d9193409868673ce68d
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-03-17 12:05:07 +1100

    instant-meeting-done

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,32 +1,71 @@
-import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '@radix-ui/react-dialog'
-import React from 'react'
-import { Button } from './ui/button'
-import { Input } from './ui/input'
+"use client";
+import { ReactNode } from "react";
+import { Dialog, DialogContent } from "./ui/Dialog";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
-const MeetingModal = () => {
-  return (
-   <Dialog>
-    
-      <DialogTrigger asChild>
-        <Button variant="outline">Share</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-md">
-       
-        <div className="flex items-center gap-2">
-          <div className="grid flex-1 gap-2">
-           
-            <Input
-              id="link"
-              defaultValue="https://ui.shadcn.com/docs/installation"
-              readOnly
-            />
-          </div>
-        </div>
-        
-      </DialogContent>
-    </Dialog>
-   
-  )
+interface MeetingModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  className?: string;
+  children?: ReactNode;
+  handleClick?: () => void;
+  buttonText?: string;
+  instantMeeting?: boolean;
+  image?: string;
+  buttonClassName?: string;
+  buttonIcon?: string;
 }
 
-export default MeetingModal
+const MeetingModal = ({
+  isOpen,
+  onClose,
+  title,
+  className,
+  children,
+  handleClick,
+  buttonText,
+  instantMeeting,
+  image,
+  buttonClassName,
+  buttonIcon,
+}: MeetingModalProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+        <div className="flex flex-col gap-6">
+          {image && (
+            <div className="flex justify-center">
+              <Image src={image} alt="checked" width={72} height={72} />
+            </div>
+          )}
+          <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
+            {title}
+          </h1>
+          {children}
+          <Button
+            className={
+              "bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0"
+            }
+            onClick={handleClick}
+          >
+            {buttonIcon && (
+              <Image
+                src={buttonIcon}
+                alt="button icon"
+                width={13}
+                height={13}
+              />
+            )}{" "}
+            &nbsp;
+            {buttonText || "Schedule Meeting"}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MeetingModal;
