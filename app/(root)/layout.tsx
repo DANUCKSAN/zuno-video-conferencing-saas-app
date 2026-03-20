c08aa0c7cdf1e8bd11bb7d903fc4c1f8d1d8dcfa
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-03-20 15:26:44 +1100

    upcoming-meeting-page-init

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider';
+import { Metadata } from 'next';
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: "Zuno",
+  description: "Video conference App",
+  icons: {
+    icon: "/icons/logo2.svg",
+  },
+};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
