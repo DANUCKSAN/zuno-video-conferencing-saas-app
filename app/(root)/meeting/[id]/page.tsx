a18815c00d91f5477a286acd2deaad087e5d06e1
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-03-20 12:11:29 +1100

    meeting-setup-half-completed

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,23 +1,49 @@
 'use client';
+
+import { useState } from 'react';
 import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-import React from 'react'
+import { useParams } from 'next/navigation';
+import { Loader } from 'lucide-react';
 
-const Meeting = ({ params }: { params: { id: string } }) => {
-  const {user, isLoaded} = useUser();
-  const [isSetupComplete, setIsSetupComplete] = React.useState(false);
+import { useGetCallById } from '@/hooks/useGetCallbyId';
+
+import MeetingSetup from '@/components/MeetingSetup';
+import MeetingRoom from '@/components/MeetingRoom';
+
+const MeetingPage = () => {
+  const { id } = useParams();
+  const { isLoaded, user } = useUser();
+  const { call, isCallLoading } = useGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call) return (
+    <p className="text-center text-3xl font-bold text-white">
+      Call Not Found
+    </p>
+  );
+
+  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowed) return ;
+
   return (
-    <main className='h-screen w-full'>
-      <StreamCall>
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
         <StreamTheme>
-          {!isSetupComplete ? (
-            'MeetingSetup'):(
-              'Meeting Room'
-            )}
+
+        {!isSetupComplete ? (
+          <MeetingSetup />
+        ) : (
+          <MeetingRoom />
+        )}
         </StreamTheme>
       </StreamCall>
     </main>
-  )
-}
+  );
+};
 
-export default Meeting;
+export default MeetingPage;
